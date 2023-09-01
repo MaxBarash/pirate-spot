3798f1d004dcfecff2bd5f93aa3ea6d347ecbf6b
DesperadoUa <Lazarev-konstant@mail.ru>
2023-09-01 17:19:05 +0300

    Add common script

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Pirate spot')
+const DOMAIN_URL = 'https://pirate-spot.lenddev.com.ua'
+const version = Date.now()
+{
+    function loadScriptDI(){
+        const injection = [
+            '/components/common/script.js'
+        ]
+        let timeCounter = 0
+        injection.forEach(item => {
+            const element = document.createElement("script")
+            element.src = `${DOMAIN_URL}${item}?v=${version}`
+            timeCounter += 300
+            setTimeout(()=>{
+                document.querySelector('body').appendChild(element)
+            }, timeCounter)
+        })
+    }
+    loadScriptDI()
+}
