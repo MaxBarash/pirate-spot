f7817d4fcab54f3f5a9e0f2fc781e819eba2ee67
DesperadoUa <Lazarev-konstant@mail.ru>
2023-09-04 12:17:26 +0300

    Add prettier in project

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const DOMAIN_URL = 'https://pirate-spot.lenddev.com.ua'
 const version = Date.now()
 {
-    function loadScriptDI(){
-        const injection = [
-            'common'
-        ]
-        injection.forEach(item => {
-            const element = document.createElement("script")
-            element.src = `${DOMAIN_URL}/components/${item}/script.js?v=${version}`
-            document.querySelector('body').appendChild(element)
-        })
-    }
-    loadScriptDI()
+	const scriptsJs = ['common']
+	function loadScriptDI(arrScripts) {
+		arrScripts.forEach(item => {
+			const element = document.createElement('script')
+			element.src = `${DOMAIN_URL}/components/${item}/script.js?v=${version}`
+			document.querySelector('body').appendChild(element)
+		})
+	}
+	loadScriptDI(scriptsJs)
+
+	const scriptsCss = ['common']
+	function loadStyleDI(arrScripts) {
+		arrScripts.forEach(item => {
+			document
+				.querySelector('body')
+				.insertAdjacentElement(
+					'beforeend',
+					`<link rel='stylesheet' href='${DOMAIN_URL}/components/${item}/style.css?v=${version}'>`
+				)
+		})
+	}
+	loadStyleDI(scriptsCss)
 }
