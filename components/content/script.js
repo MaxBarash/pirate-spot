429c66b29436509e2527e2a2e862967893a3a4cc
DesperadoUa <Lazarev-konstant@mail.ru>
2023-09-11 16:59:52 +0300

    Clear content script

--- components/content/script.js
+++ components/content/script.js
@@ -2,7 +2,6 @@
 	const btn = document.querySelector('.ps_content_read_more')
 	const container = document.querySelector('.ps_content_container_read_more')
 	if (btn && container) {
-		console.log('content script')
 		btn.addEventListener('click', () => {
 			container.classList.add('ps_show')
 		})
